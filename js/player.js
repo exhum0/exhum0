07945a04f18205c7f9457e2e0a269f707ad87d9b
Dmitry Lean <adam.lean.92@gmail.com>
2025-06-30 13:14:48 +0800

    .

--- js/player.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.play-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const player = document.getElementById('player');
-    player.src = '/audio/mippo.mp3'; // Подставляй путь динамически
-    player.play();
-  });
-});
