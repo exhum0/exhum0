fb7949019aaa9f8190008940c07c3fd89cf5b781
DM!TRY <adam.lean.92@gmail.com>
2025-08-15 14:41:41 +0800

    .

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
+const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.string(),
-    author: z.string(),
     image: z.string().optional(),
-    lang: z.enum(["ru", "eng"]), // чтобы пост знал свой язык
+    lang: z.enum(["ru", "en"])
   }),
 });
 
 export const collections = {
-  blog,
+  blog: blogCollection,
 };
