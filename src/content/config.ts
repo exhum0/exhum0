de4e015f8159e7a10aa77d0285de2cfcf93b0652
DM!TRY <adam.lean.92@gmail.com>
2025-08-16 01:26:13 +0800

    .

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     date: z.date(),
-    description: z.string(),
+    description: z.string().optional(),
+    tag: z.string().optional(),
+    image: z.string().optional(),
   }),
 });
 
-export const collections = {
-  blog,
-};
+export const collections = { blog };
