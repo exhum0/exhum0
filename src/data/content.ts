0e86048c3f122c5d64cb7597febd83b8afd1d674
DM!TRY <adam.lean.92@gmail.com>
2025-08-15 09:32:55 +0800

    .

---
+++ src/data/content.ts
@@ -0,0 +1,23 @@
+export const aboutData = {
+  imageSrc: "/images/avatar.jpg",
+  imageAlt: "Моё фото",
+  title: "Обо мне",
+  subtitle: "Frontend-разработчик",
+  paragraphs: [
+    "Занимаюсь веб-разработкой с 2015 года.",
+    "Люблю Astro и TypeScript.",
+  ],
+  contactLink: {
+    href: "https://t.me/dg404_bot",
+    text: "Связаться",
+  },
+};
+
+export const skillsData = {
+  title: "Навыки",
+  skills: [
+    { name: "HTML", icon: "/icons/html.svg" },
+    { name: "CSS", icon: "/icons/css.svg" },
+    { name: "JavaScript", icon: "/icons/js.svg" },
+  ],
+};
