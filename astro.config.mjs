1d92dc80ec0fa34e19dbed6bdf599cb6f62bb3f8
DM!TRY <adam.lean.92@gmail.com>
2025-08-15 16:39:53 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,3 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
-
-   import { defineConfig } from 'astro/config';
-   import mdx from '@astrojs/mdx';
-
-   export default defineConfig({
-     integrations: [mdx()]
-   });
